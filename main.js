eb425810b4397fc2294360164b15c867135d257c
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2022-04-21 22:19:15 -0700

    Included navbar functionality

--- main.js
+++ main.js
@@ -5,6 +5,27 @@ document.addEventListener('mousemove', (e) => {
     cursor.style.top = e.pageY + 'px';
 })
 
+var tabsNewAnim = $('#navbar-animmenu');
+		var selectorNewAnim = $('#navbar-animmenu').find('li').length;
+		//var selectorNewAnim = $(".tabs").find(".selector");
+		var activeItemNewAnim = tabsNewAnim.find('.active');
+		var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+		var itemPosNewAnimLeft = activeItemNewAnim.position();
+		$(".hori-selector").css({
+			"left":itemPosNewAnimLeft.left + "px",
+			"width": activeWidthNewAnimWidth + "px"
+		});
+		$("#navbar-animmenu").on("click","li",function(e){
+			$('#navbar-animmenu ul li').removeClass("active");
+			$(this).addClass('active');
+			var activeWidthNewAnimWidth = $(this).innerWidth();
+			var itemPosNewAnimLeft = $(this).position();
+			$(".hori-selector").css({
+				"left":itemPosNewAnimLeft.left + "px",
+				"width": activeWidthNewAnimWidth + "px"
+			});
+		});
+
 $(document).ready(function() {
 	$("#fullpage").fullpage({
 		anchors: ["firstPage", "secondPage", "thirdPage"],
