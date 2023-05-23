1f698017324c9ab024cc6a6adbebb218610c36b4
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-05-23 14:44:56 -0700

    Header footer and loader components

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -2,20 +2,20 @@
 import Link from 'next/link';
 import React from 'react';
 
-const NavBar: React.FC = () => (
+const Header: React.FC = () => (
   <nav className="bg-blue-500">
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between h-16">
         <div className="flex items-center">
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
-              <Link href="/">
+              <Link href="/about-me">
                 <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
               </Link>
-              <Link href="/about">
+              <Link href="/blog">
                 <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
               </Link>
-              <Link href="/contact">
+              <Link href="/projects">
                 <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
               </Link>
             </div>
@@ -26,4 +26,4 @@ const NavBar: React.FC = () => (
   </nav>
 );
 
-export default NavBar;
+export default Header;
