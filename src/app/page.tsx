1c5ee69764dba45f6689972981bed0727cada18e
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-05-23 13:09:12 -0700

    Migrated to source folder

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
+import NavBar from '../components/NavBar';
+
+export default function Page() {
+  return (
+    <div>
+      {/* <NavBar /> */}
+      <h1>Hello</h1>
+    </div>
+  );
+}
