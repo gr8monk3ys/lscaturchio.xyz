006df5905b73930bbab75b17a50d84c31a22452f
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-05-24 14:34:38 -0700

    Added sections to the website

---
+++ components/sections/PostSection.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { PostMetadata } from "../PostMetadata";
+
+const PostSection = (props: PostMetadata) => {
+  return (
+    <div
+      className="border border-slate-300 p-4 rounded-md shadow-sm
+    bg-white"
+    >
+      <p className="text-sm text-slate-400">{props.date}</p>
+
+      <Link href={`/posts/${props.slug}`}>
+        <h2 className=" text-violet-600 hover:underline mb-4">{props.title}</h2>
+      </Link>
+      <p className="text-slate-700">{props.subtitle}</p>
+    </div>
+  );
+};
+
+export default PostSection;
