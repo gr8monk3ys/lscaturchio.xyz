666ac14634b0542db0ce61fe57ca92d0a0e5446d
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2023-05-29 14:21:41 -0700

    Styled the skill section to look nicer

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Skill: React.FC<{ skill: string }> = ({ skill }) => {
   return (
-    <p className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold">
+    <p className="inline-flex items-center bg-blue-500 px-3 py-1 mr-2 mt-2 text-white rounded-md font-semibold text-sm tracking-wide shadow-md hover:shadow-lg transition-shadow duration-300">
       {skill}
     </p>
   );
