0a7d6255338178c41a57b271acb97a3ae74459c3
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2023-05-29 23:29:42 -0700

    Modified sections styling

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,39 +1,25 @@
 'use client';
 import React from 'react';
-import Image from 'next/image';
+import Image from 'next/legacy/image';
 import {HiArrowDown} from 'react-icons/hi';
 import ScrollLink from '../ScrollLink';
 
 const HeroSection: React.FC = () => {
   return (
-    <section id="home">
-      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
-        <div className="md:mt-2 md:w-1/2">
-          <Image
-            src="/headshot.png"
-            alt="Lorenzo's Headshot"
-            width={325}
-            height={325}
-            className="rounded-full shadow-2xl"
-          />
+    <section id="home" className="my-10 py-10 md:py-20">
+      <div className="container mx-auto px-6 sm:px-12 md:px-20">
+        <div className="flex flex-col items-center justify-center animate-fadeIn animation-delay-2 md:flex-row md:justify-between">
+          <div className="w-32 h-32 md:w-64 md:h-64 overflow-hidden rounded-full shadow-2xl mb-8 md:mb-0 relative">
+            <Image src="/headshot.png" alt="Lorenzo's Headshot" layout="fill" objectFit="cover" className="absolute" />
+          </div>
+          <div className="md:w-1/2">
+            <h1 className="text-3xl font-bold mt-6 md:text-5xl md:mt-0"> I&#39;m Lorenzo</h1>
+            <p className="text-md mt-4 md:text-lg">
+              I&#39;m a <span className="font-semibold text-blue-600">Data Science Student </span>
+              based in Los Angeles, CA. Working on automating my life
+            </p>
+          </div>
         </div>
-        <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Lorenzo!</h1>
-          <p className="text-lg mt-4 mb-6 md:text-2xl">
-            I&#39;m a <span className="font-semibold text-blue-600">Data Science Student </span>
-            based in Los Angeles, CA. Working on automating my life
-          </p>
-          <ScrollLink
-            to="projects"
-            className="text-neutral-100 font-semibold px-6 py-3 bg-blue-600 rounded shadow hover:bg-blue-700">
-            Projects
-          </ScrollLink>
-        </div>
-      </div>
-      <div className="flex flex-row items-center text-center justify-center">
-        <ScrollLink to="about" className="">
-          <HiArrowDown size={35} className="animate-bounce" />
-        </ScrollLink>
       </div>
     </section>
   );
