c2e3a6ab5c3710766122ecc0b6d691e983a5dc13
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-05-31 14:26:25 -0700

    Blur component and added styling to tailwind config

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,15 +1,1 @@
-self.__RSC_CSS_MANIFEST={
-  "cssImports": {
-    "C:\\Users\\loren\\Code\\gr8monk3ys.github.io\\app\\layout.tsx": [
-      "C:\\Users\\loren\\Code\\gr8monk3ys.github.io\\app\\globals.css"
-    ]
-  },
-  "cssModules": {
-    "C:\\Users\\loren\\Code\\gr8monk3ys.github.io\\app\\page": [
-      "C:\\Users\\loren\\Code\\gr8monk3ys.github.io\\app\\globals.css"
-    ],
-    "C:\\Users\\loren\\Code\\gr8monk3ys.github.io\\app\\posts\\[slug]\\page": [
-      "C:\\Users\\loren\\Code\\gr8monk3ys.github.io\\app\\globals.css"
-    ]
-  }
-}
+self.__RSC_CSS_MANIFEST={"cssImports":{"C:\\Users\\JoeG&M\\Downloads\\gr8monk3ys.github.io\\app\\layout.tsx":["C:\\Users\\JoeG&M\\Downloads\\gr8monk3ys.github.io\\app\\globals.css"]},"cssModules":{"C:\\Users\\JoeG&M\\Downloads\\gr8monk3ys.github.io\\app\\page":["C:\\Users\\JoeG&M\\Downloads\\gr8monk3ys.github.io\\app\\globals.css"],"C:\\Users\\JoeG&M\\Downloads\\gr8monk3ys.github.io\\app\\posts\\[slug]\\page":["C:\\Users\\JoeG&M\\Downloads\\gr8monk3ys.github.io\\app\\globals.css"]}}
