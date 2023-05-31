e843baddfac847cf9b068a453a4f7ff62b63e337
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-05-31 14:32:09 -0700

    Formatting

--- components/blur.tsx
+++ components/blur.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Blur = () => {
   return (
-    <div className="bg-gray-50 min-h-screen flex items-center justify-center px-16">
+    <div className="bg-white-50 min-h-screen flex items-center justify-center px-16">
       <div className="relative w-full max-w-lg">
         <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
         <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
