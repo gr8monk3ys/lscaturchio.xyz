48f1f09949181d8327554c7036388886a9df2d56
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-06-06 23:09:40 -0700

    Updated about section info

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -44,7 +44,7 @@ export default function About() {
         <title>About - Lorenzo Scaturchio</title>
         <meta
           name="description"
-          content="I’m Lorenzo Scaturchio. I live in New York City, where I design the future."
+          content="I’m Lorenzo Scaturchio. I live in Los Angeles California, where I try to automate my life."
         />
       </Head>
       <Container className="mt-16 sm:mt-32">
@@ -61,58 +61,55 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Lorenzo Scaturchio. I live in New York City, where I design the
-              future.
+              I’m Lorenzo Scaturchio. I live in Los Angeles California, where I try to automate my life.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                Growing up, I was always fascinated with the way things worked.
+                There was nothing that I couldn't set my mind to that I would try
+                to figure out. I was always taking things apart and putting them
+                back together. Failed a bunched but learned along the way.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                The only thing I loved more than computers as a kid was music.
+                At an early age I learned to play piano by ear and from there I
+                got a guitar. It has been a passion of mine ever since and some
+                day I will make an album that I will publish on here and many 
+                other platforms.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                I spent the next few summers indoors not progressing but 
+                still being passionate about learning something. I always
+                had to be busy so there was always a new hobby that I would
+                pick up, for better or for worse.
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                Today, I’m creating projects to make my life easier, where I 
+                am working with AI / ML so that the next generation can
+                inspire to innovate with the new tools provided to us.
               </p>
             </div>
           </div>
           <div className="lg:pl-20">
             <ul role="list">
-              <SocialLink href="#" icon={TwitterIcon}>
+              {/* <SocialLink href="#" icon={TwitterIcon}>
                 Follow on Twitter
-              </SocialLink>
-              <SocialLink href="#" icon={InstagramIcon} className="mt-4">
+              </SocialLink> */}
+              <SocialLink href="https://www.instagram.com/lorenzo.scaturchio/" icon={InstagramIcon} className="mt-4">
                 Follow on Instagram
               </SocialLink>
-              <SocialLink href="#" icon={GitHubIcon} className="mt-4">
+              <SocialLink href="https://github.com/gr8monk3ys" icon={GitHubIcon} className="mt-4">
                 Follow on GitHub
               </SocialLink>
-              <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+              <SocialLink href="https://www.linkedin.com/in/lorenzo-scaturchio/" icon={LinkedInIcon} className="mt-4">
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
-                href="mailto:spencer@planetaria.tech"
+                href="mailto:lorenzosca7@gmail.com"
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
-                spencer@planetaria.tech
+                lorenzosca7@gmail.com
               </SocialLink>
             </ul>
           </div>
