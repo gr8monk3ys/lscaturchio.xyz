4440ff194978f7f9ebb3a69848244e8f2d9303fd
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-06-07 11:42:37 -0700

    Formatted documents

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,7 @@ export function Footer() {
   return (
     <footer className="mt-32">
       <Container.Outer>
-        <div className="border-t border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40">
+        <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
           <Container.Inner>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
