05ece30737361230539937784895e28c90499abc
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-06-07 14:51:00 -0700

    Created a production build

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -41,12 +41,12 @@ export default function ArticlesIndex({ articles }) {
         <title>Articles - Lorenzo Scaturchio</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content="All of my long-form thoughts on programming, philosophy, music, or whatever I find interesting."
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="Writing on software design, company building, and school."
+        intro="All of my long-form thoughts on programming, philosophy, music, or whatever I find interesting."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
