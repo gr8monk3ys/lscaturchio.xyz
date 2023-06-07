1465325c06a72c7fbde3ec23ca056f9a734ca1e0
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-06-07 11:35:59 -0700

    More styling changes

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -90,7 +90,7 @@ export default function Projects() {
                 <Card.Link href={project.link.href}>{project.name}</Card.Link>
               </h2>
               <Card.Description>{project.description}</Card.Description>
-              <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+              <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-blue-500 dark:text-zinc-200">
                 <LinkIcon className="h-6 w-6 flex-none" />
                 <span className="ml-2">{project.link.label}</span>
               </p>
