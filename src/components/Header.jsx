e5f8e66ade5fb79cc30ed99cc4c6acb1686c6601
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-06-06 23:09:18 -0700

    Minor fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -165,7 +165,7 @@ function DesktopNavigation(props) {
         <NavItem href="/about">About</NavItem>
         <NavItem href="/articles">Articles</NavItem>
         <NavItem href="/projects">Projects</NavItem>
-        <NavItem href="/speaking">Speaking</NavItem>
+        {/* <NavItem href="/speaking">Speaking</NavItem> */}
         <NavItem href="/uses">Uses</NavItem>
       </ul>
     </nav>
