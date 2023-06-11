170d3bdab33875c35deebd78e1bb6cf1f8f04bb9
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-06-11 16:34:12 -0700

    Grammatical fixes

--- src/pages/uses.jsx
+++ src/pages/uses.jsx
@@ -29,68 +29,71 @@ export default function Uses() {
   return (
     <>
       <Head>
-        <title>Uses - Lorenzo Scaturchio</title>
+        <title>Tools & Recommendations - Lorenzo Scaturchio</title>
         <meta
           name="description"
-          content="Software I use, and other things I recommend."
+          content="A compilation of the software, gadgets, and other resources I frequently use and highly recommend."
         />
       </Head>
       <SimpleLayout
-        title="Software I use, gadgets I love, and other things I recommend."
-        intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+        title="My Arsenal of Software, and Favourite Gadgets"
+        intro="Here are the tools I use to develop software, the methods I use to stay productive, or the items I purchase that serve as a comforting illusion of productivity when I am actually procrastinating. Here is a comprehensive list of all the things I love and use."
       >
         <div className="space-y-20">
-          <ToolsSection title="Workstation">
+          <ToolsSection title="Workstation Essentials">
             <Tool title="Custom PC (2018), Intel i7 7700k, Nvidia RTX 3090, 64GB DDR4 RAM, 1000 W Power supply">
-              My custom PC has been my bread and butter for a while. The specs
-              are from a long time of owning and upgrading my computer.
+              My custom-built PC has been my faithful companion for a
+              considerable period. The specifications reflect a long journey of
+              owning and continually upgrading my system.
             </Tool>
             <Tool title="Samsung Crg9 Ultrawide display 120hz">
-              This display lets me have just one screen but it can act like two
-              which couldnt make me any happier.
+              This display provides the convenience of two screens in one, a
+              feature that never fails to bring me immense satisfaction.
             </Tool>
             <Tool title="Custom  Glorious Gaming Keyboard">
-              My custom keyboard was built to be extremely silent. I chose the
-              Gateron Silent Red switches as well as lubed all the keys to
-              ensure minimal sound from typing. Basically I am a loud typer so I
-              wanted to reduce that with the keyboard.
+              My custom keyboard was designed for the utmost silence. I opted
+              for Gateron Silent Red switches and meticulously lubed all the
+              keys to minimize typing noise. As a naturally loud typer, this
+              keyboard is a sound-reducing asset.
             </Tool>
             <Tool title="Razer Viper Ulitimate Hyperspeed Lightweight Mouse">
-              This mouse is absurdly light and I just love how I could drag
-              around with it if I am playing a game or if I am playing bullet
-              chess, it does not feel like I am holding anything at all.
+              The incredible lightness of this mouse is nothing short of
+              amazing. Whether I am gaming or playing bullet chess, the
+              negligible weight makes it feel as if I am not holding anything at
+              all.
             </Tool>
             <Tool title="Herman Miller Sayl Chair">
-              I am in love with this chair and it frankly makes up most of the
-              aesthetic
+              I have fallen in love with this chair, and it significantly
+              contributes to the overall aesthetic of my workspace.
             </Tool>
           </ToolsSection>
-          <ToolsSection title="Development tools">
+          <ToolsSection title="Essential Development Tools">
             <Tool title="Neovim (NvChad)">
-              This is a suite of plugins for an already amazing IDE that are
-              added onto nvim that I absolutely love and cant really go back to
-              any other IDE since it works so well and is pretty efficient.
+              This is a suite of plugins for the already impressive Neovim IDE.
+              The efficiency and effectiveness of these plugins make it
+              impossible for me to revert to any other IDE.
             </Tool>
             <Tool title="Arch Linux">
-              I dont want to be that guy... but I run Arch btw but I am that
-              guy. Just started using this instead of ubuntu for my WSL
-              installation and I do enjoy using pacman more than apt-get. Plus
-              their wiki is just insane.
+              I might be falling into a stereotype here, but I run Arch.
+              Recently, I switched from Ubuntu to Arch for my WSL installation
+              and have found the use of pacman far more enjoyable than apt-get.
+              Not to mention, the Arch wiki issimply phenomenal.
             </Tool>
           </ToolsSection>
-          <ToolsSection title="Design">
+          <ToolsSection title="Design Resources">
             <Tool title="Figma">
-              Figma has been really useful if I want to design something really
-              quick like a basic wireframe. Other than that I rarely ever use
-              this.
+              Figma proves to be a handy tool whenever I need to design
+              something quickly, such as a basic wireframe. Beyond that, I do not
+              use it extensively, but I appreciate its availability when needed.
             </Tool>
           </ToolsSection>
-          <ToolsSection title="Productivity">
+          <ToolsSection title="Productivity Boosters">
             <Tool title="Notion">
-              I use this for practially everything in my life. From setting up
-              the weekly workouts, to to-do lists, to my recipe book, this has
-              all of the organization that I would need and no I do not consider
-              it a second brain but it is very useful.
+              Notion is my go-to for virtually everything in my life. From
+              planning my weekly workouts, to maintaining to-do lists, to
+              keeping my recipe book, it offers all the organization I need.
+              While I would not classify it as a second brain, its usefulness is
+              undeniable.
             </Tool>
           </ToolsSection>
         </div>
