e77f71fd20bac48535c34969c49140f08c3b2cde
gr8monk3ys <lorenzosca7@gmail.com>
2025-01-16 10:57:06 -0800

    Add extra socials

--- src/constants/socials.tsx
+++ src/constants/socials.tsx
@@ -2,6 +2,10 @@ import {
   IconBrandLinkedin,
   IconBrandGithubFilled,
   IconBrandInstagram,
+  IconBook,
+  IconMovie,
+  IconBrandLeetcode,
+  IconBookmarks,
 } from "@tabler/icons-react";
 
 export const socials = [
@@ -19,5 +23,25 @@ export const socials = [
     href: "https://www.instagram.com/lorenzo.scaturchio",
     label: "Instagram",
     icon: IconBrandInstagram,
+  },
+  {
+    href: "https://letterboxd.com/gr8monk3ys/",
+    label: "Letterboxd",
+    icon: IconMovie,
+  },
+  {
+    href: "https://www.goodreads.com/user/show/168274083-lorenzo",
+    label: "Goodreads",
+    icon: IconBook,
+  },
+  {
+    href: "https://leetcode.com/u/gr8monk3ys/",
+    label: "Leetcode",
+    icon: IconBrandLeetcode,
+  },
+  {
+    href: "https://substack.com/@gr8monk3ys",
+    label: "Substack",
+    icon: IconBookmarks,
   },
 ];
