8ae19ca576e6ab04ecd450598d578a905ed9d26b
gr8monk3ys <lorenzosca7@gmail.com>
2025-01-16 14:40:27 -0800

    Update footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { twMerge } from "tailwind-merge";
-import { Footer } from "@/components/Footer";
+import { Footerdemo } from "@/components/ui/footer-section";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -33,7 +33,7 @@ export default function RootLayout({
         <div className="lg:pl-2 lg:pt-2 bg-gray-100 flex-1 overflow-y-auto">
           <div className="flex-1 bg-white min-h-screen lg:rounded-tl-xl border border-transparent lg:border-neutral-200 overflow-y-auto">
             {children}
-            <Footer />
+            <Footerdemo />
           </div>
         </div>
       </body>
