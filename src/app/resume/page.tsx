5a3d17476c89afd1da4b7de93a014c7ab28cce54
gr8monk3ys <lorenzosca7@gmail.com>
2025-01-16 14:40:11 -0800

    Update resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,19 +4,13 @@ import { Highlight } from "@/components/Highlight";
 import { Paragraph } from "@/components/Paragraph";
 import { Products } from "@/components/Products";
 import { WorkHistory } from "@/components/WorkHistory";
+import { WorkTimeline } from "@/components/ui/work-timeline";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <Container>
-      <span className="text-4xl">💼</span>
-      <Heading className="font-black">Work History</Heading>
-      <Paragraph className="max-w-xl mt-4">
-        I&apos;m a Generative AI Engineer that loves{" "}
-        <Highlight>automating my life</Highlight> and models that can impact
-        people&aposs lives
-      </Paragraph>
-      <WorkHistory />
+      <WorkTimeline />
     </Container>
   );
 }
