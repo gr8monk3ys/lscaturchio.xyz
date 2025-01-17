4f06f15a2233c94ba97d236f841380c69583b33d
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2025-01-16 21:37:34 -0800

    Bug fix

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,23 @@
+import { Container } from "@/components/Container";
+import { Heading } from "@/components/Heading";
+import { Paragraph } from "@/components/Paragraph";
+import { ContactCTA } from "@/components/ui/contact-cta";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact | Lorenzo Scaturchio",
+  description:
+    "Lorenzo Scaturchio is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+};
+
+export default function Contact() {
+  return (
+    <Container>
+      <Heading className="font-black mb-2">Let&apos;s Connect</Heading>
+      <Paragraph className="mb-10 max-w-xl">
+        I&apos;m always open to discussing new projects, creative ideas, or opportunities to be part of your visions.
+      </Paragraph>
+      <ContactCTA />
+    </Container>
+  );
+}
