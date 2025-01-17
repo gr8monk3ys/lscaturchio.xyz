ffc57127d83550a584b34e21514ae9a924a9ff2f
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2025-01-16 21:34:04 -0800

    Update project dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,8 @@ const {
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
