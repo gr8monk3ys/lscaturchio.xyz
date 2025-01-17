d7948ac7f9856c6bffbb4abcf1709afb42633917
gr8monk3ys <lorenzosca7@gmail.com>
2025-01-17 08:34:37 -0800

    Got rid of waves and background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,17 +30,17 @@ export default function RootLayout({
       <body className="min-h-screen antialiased">
         <CustomCursor />
         <MouseParticles />
-        <DynamicBackground />
+        {/* <DynamicBackground /> */}
         <Navbar />
         <div className="flex-1 relative">
           <div className="container mx-auto px-4 py-6 md:px-6 lg:px-8">
             <main className="relative">
               {children}
+              <SpeedInsights />
             </main>
             <Footer />
           </div>
         </div>
-        <SpeedInsights />
       </body>
     </html>
   );
