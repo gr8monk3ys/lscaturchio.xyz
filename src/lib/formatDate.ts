31a06c03caa12b73874bee527ce3c6b04cd04497
gr8monk3ys <lorenzosca7@gmail.com>
2025-01-22 13:21:18 -0800

    chore: Add environment configuration files

---
+++ src/lib/formatDate.ts
@@ -0,0 +1,10 @@
+export function formatDate(date: Date): string {
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+  }).format(date);
+}
